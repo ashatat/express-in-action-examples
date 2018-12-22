0d027fc69a2df3deaa08a80641e68ff2b4ff8d9a
ahmed shatat <a.shatat@hotmail.com>
2018-12-22 22:08:19 +0200

    initial setup static-file-fun

---
+++ static-file-fun/app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var path = require("path");
+var fs = require("fs");
+
+const app = express();
+
+app.use(function(req, res, next) {
+  console.log("Request IP: " + req.url);
+  console.log("Request date: " + new Date());
+});
+
+app.listen(3000, function() {
+  console.log("App started on port 3000");
+});
