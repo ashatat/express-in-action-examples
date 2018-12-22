888506803713061df5b28c8c9ad825e6ab0b0163
ahmed shatat <a.shatat@hotmail.com>
2018-12-22 11:30:26 +0200

    feat: add logging middleware.
    -
    -
    -

--- hello-world/app.js
+++ hello-world/app.js
@@ -3,6 +3,11 @@ const http = require('http');
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('In comes a '+ req.method + ' to ' + req.url);
+  next();
+});
+
 app.use((req, res) => {
   console.log('in comes a request to: ' + req.url);
   res.end('Hello, world!');
