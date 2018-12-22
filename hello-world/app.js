2458bda9bf02d917f6387f22871a9c85beb0f317
ahmed shatat <a.shatat@hotmail.com>
2018-12-22 11:37:51 +0200

    feat: add authentication middleware.
    only users logged in even minutes are allowed
    -
    -
    -

--- hello-world/app.js
+++ hello-world/app.js
@@ -8,6 +8,16 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use((req, res, next) => {
+  const minute = (new Date()).getMinutes();
+  if (minute%2 === 0) {
+    next();
+  } else {
+    res.statusCode = 403;
+    res.end('Not authorized.');
+  }
+});
+
 app.use((req, res) => {
   console.log('in comes a request to: ' + req.url);
   res.end('Hello, world!');
