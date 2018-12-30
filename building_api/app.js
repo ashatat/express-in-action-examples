2fd30591642f9202dfd083cee889e96fbc8ed50b
Ahmed Shatat <a.shatat@hotmail.com>
2018-12-30 18:24:00 +0200

    fix: change max, min to Number type

--- building_api/app.js
+++ building_api/app.js
@@ -3,14 +3,18 @@ const express = require('express');
 const app = express();
 
 app.get('/random/:min/:max', (req, res) => {
-    const { min, max } = req.params;
+    let { min, max } = req.params;
+
+    max = Number(max);
+    min = Number(min);
 
     if (isNaN(min) || isNaN(max)) {
         res.status(404).json({ error: 'Bad request.'});
         return;
     }
 
-    const result = Math.round((Math.random() * (max - min)) + min);
+    const randomNum = Math.random();
+    const result = Math.round((randomNum * (max - min)) + min);
     res.json({ result });
 });
 
